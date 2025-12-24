4df9e81138109c64d0a3c3bfe0623679bbf6c5dd
jeroenyolo <jeroen@typetone.ai>
2025-12-24 10:17:40 +0100

    fix prisma

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+import type { PrismaClient as PrismaClientType } from '@prisma/client';
+
+const { PrismaClient } = pkg;
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
 declare global {
   // eslint-disable-next-line no-var
-  var __prisma: PrismaClient | undefined;
+  var __prisma: PrismaClientType | undefined;
 }
 
-function createPrismaClient(): PrismaClient {
+function createPrismaClient(): PrismaClientType {
   const connectionString = process.env.DATABASE_URL;
   
   if (!connectionString) {
